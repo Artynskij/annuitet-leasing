02d5b7f9170a14ed9998e91d741563a5f2c7b777
artem <aartynskij0001@gmail.com>
2025-01-23 14:31:31 +0300

    feate: add diff

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const resultBlock = document.querySelector(".result-ctn");
 const table = resultBlock.querySelector("table");
 const buttonCalcResult = document.querySelector(".calc-result");
 const buttonDownload = document.querySelector("#table-download");
+
 buttonDownload.addEventListener("click", (e) => {
   if (!buttonDownload.href) {
     e.preventDefault();
@@ -10,60 +11,14 @@ buttonDownload.addEventListener("click", (e) => {
     return;
   }
 });
-function downloadFile(tableData) {
-  function convertToCSV(data) {
-    const headers = [
-      "месяц",
-      "Лизинговый платеж с НДС",
-      "Гашение основного долга без НДС",
-      "Гашение основного долга - НДС",
-      "Interest Payment (With NDS)",
-      "Вознаграждение лизингодателя без ндс",
-      "Вознаграждение лизингодателя - НДС",
-      "Principal Payment (With NDS)",
-      "Величина платежа без НДС",
-      "Величина платежа - НДС",
-      "Остаток стоиомсти предмета",
-    ];
-    
 
-    const rows = data.map((item) => [
-      item.month,
-      item.monthlyPayment?.withNds || 0,
-      item.monthlyPayment?.value || 0,
-      item.monthlyPayment?.nds || 0,
-      item.interestPayment?.withNds || 0,
-      item.interestPayment?.value || 0,
-      item.interestPayment?.nds || 0,
-      item.principalPayment?.withNds || 0,
-      item.principalPayment?.value || 0,
-      item.principalPayment?.nds || 0,
-      item.balance || 0,
-    ]);
-
-    const csvContent = [headers, ...rows]
-      .map((row) => row.join(";"))
-      .join("\n");
-
-    return `\uFEFF${csvContent}`;
-  }
-  function downloadFile(content, contentType) {
-    const blob = new Blob([content], { type: contentType });
-    const inputName = document.querySelector("#name-input");
-    const fileName = inputName.value ? inputName.value : "линговый договорчик";
-    buttonDownload.href = URL.createObjectURL(blob);
-    buttonDownload.download = `${fileName}.csv`;
-  }
-  const csvContent = convertToCSV(tableData);
-  downloadFile(csvContent, "text/csv");
-}
 // подсчет
 
 buttonCalcResult.addEventListener("click", (e) => {
   e.preventDefault();
   const dataLeasingStart = {
     sum: 0,
-    firstPayment: 20000,
+    firstPayment: 0,
     percent: 0,
     term: 0,
     redemptionPercent: 1,
@@ -71,14 +26,20 @@ buttonCalcResult.addEventListener("click", (e) => {
     condition: [],
   };
   const dataLeasing = getDataInput(dataLeasingStart);
+  const selectValue = document.querySelector("#changeSchedule").value;
 
   if (dataLeasing === "error") {
     alert("нету важных данных");
     return;
   }
-  const annuity = new Annuity({ ...dataLeasing })
-  // const schedule = leasingAnnuitySchedule({ ...dataLeasing });
-  const schedule = annuity.getSchedule();
+  let calcClass;
+  if (selectValue === "ann") {
+    calcClass = new Annuity({ ...dataLeasing });
+  } else {
+    calcClass = new Differentiated({ ...dataLeasing });
+  }
+
+  const schedule = calcClass.generateSchedule();
 
   table.innerHTML = "";
   table.appendChild(createHeadTable());
